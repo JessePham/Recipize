0e54a788fe1a00a933f843a05fd59a525da26fea
Jesse Pham <jessephams@gmail.com>
2022-11-20 15:27:29 -0800

    Take pictures with camera feature added

---
+++ frontend/lib/camera_screen_display.dart
@@ -0,0 +1,77 @@
+import 'camera_image_display.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CameraScreenDisplay extends StatefulWidget {
+  final CameraDescription camera;
+
+  const CameraScreenDisplay({Key key, @required this.camera}) : super(key:key);
+
+  @override
+  CameraScreenState createState() => CameraScreenState();
+}
+
+class CameraScreenState extends State<CameraScreenDisplay> {
+  CameraController controller;
+  Future<void> initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.camera, ResolutionPreset.medium);
+    
+    initializeControllerFuture = controller.initialize();  
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      body: FutureBuilder<void>(
+        future: initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(controller);
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        
+        child: Icon(Icons.camera_alt),
+        onPressed: () async {
+          try {
+            await initializeControllerFuture;
+
+            final path = join(
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            await controller.takePicture(path);
+
+            Navigator.push(
+              context, 
+              MaterialPageRoute(
+                builder: (context) => DisplayCameraImageScreen(imagePath: path)
+              )
+            );
+          } catch (error) {
+            print(error);
+          }
+        }
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+    );
+  }
+}
