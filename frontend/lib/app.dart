cca605f8187f95966dbcd38fc0703a0b3566d19e
Jesse Pham <jessephams@gmail.com>
2022-11-26 23:27:10 -0800

    Favorites Implementation

---
+++ frontend/lib/app.dart
@@ -0,0 +1,67 @@
+// import 'package:Recipize/camera_screen.dart';
+import 'package:Recipize/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'favorites_tab.dart';
+import 'camera_screen_display.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return CupertinoApp(
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+    ],
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.camera),
+            label: 'Camera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.bookmark),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: CameraScreenDisplay(
+                  camera: camera,
+                ),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: FavoritesListTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },
+    );
+  }
+}
---
+++ frontend/lib/recipes.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'model/app_state_model.dart';
+import 'recipe_row_item.dart';
+import 'model/recipe.dart';
+
+class Recipes extends StatefulWidget {
+  List<Recipe> recipes;
+
+  Recipes({Key key, @required this.recipes}) : super(key: key);
+
+  @override
+  _RecipesState createState() => _RecipesState();
+}
+
+class _RecipesState extends State<Recipes> {
+
+@override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateModel>(
+      builder: (context, model, child) {
+        return CustomScrollView(
+          semanticChildCount: widget.recipes.length,
+          slivers: <Widget>[
+            const CupertinoSliverNavigationBar(
+              largeTitle: Text('Recipes'),
+            ),
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 8),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    if (index < widget.recipes.length) {
+                      return RecipeRowItem(
+                          index: index,
+                          recipe: widget.recipes[index],
+                          lastItem: index == widget.recipes.length - 1);
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
